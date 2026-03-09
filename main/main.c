ff10ce542da0e66d0d27beabc7cfeecd72082913
garciah27 <garciah@union.edu>
2026-03-08 21:32:02 -0400

    updated_with_button_and_buzzer

--- main/main.c
+++ main/main.c
@@ -2,9 +2,30 @@
 #include <freertos/task.h>
 #include <hd44780.h>
 #include <esp_idf_lib_helpers.h>
+#include "driver/gpio.h"
+
+void delay_ms(int t);
+
+#define BUZZER GPIO_NUM_10
+#define BUTTON GPIO_NUM_18
 
 void app_main()
 {
+    //button and buzzer config
+    gpio_config_t btn_conf = {
+        .pin_bit_mask = (1ULL << BUTTON),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+    gpio_config(&btn_conf);
+
+    gpio_reset_pin(BUZZER);
+    gpio_set_direction(BUZZER, GPIO_MODE_OUTPUT);
+    gpio_set_level(BUZZER, 0);   // buzzer off
+
+
     vTaskDelay(pdMS_TO_TICKS(500));   // allow LCD to power up
 
     hd44780_t lcd =
@@ -13,12 +34,12 @@ void app_main()
         .font = HD44780_FONT_5X8,
         .lines = 4,
         .pins = {
-            .rs = GPIO_NUM_4,
-            .e  = GPIO_NUM_5,
-            .d4 = GPIO_NUM_6,
-            .d5 = GPIO_NUM_7,
-            .d6 = GPIO_NUM_9,
-            .d7 = GPIO_NUM_10,
+            .rs = GPIO_NUM_38,
+            .e  = GPIO_NUM_37,
+            .d4 = GPIO_NUM_36,
+            .d5 = GPIO_NUM_35,
+            .d6 = GPIO_NUM_48,
+            .d7 = GPIO_NUM_47,
             .bl = HD44780_NOT_USED
         }
     };
@@ -71,4 +92,20 @@ void app_main()
     hd44780_gotoxy(&lcd, 0, 1);     // row index 1 = second row
     hd44780_puts(&lcd, "In Progress");
 
+    //state for button and buzzer 
+    int prev_state = 1;   // 1 = not pressed
+    int curr_state = 1;
+    int buzzer_on = 0;    // start with buzzer OFF
+    
+    while (1) {
+        curr_state = gpio_get_level(BUTTON);
+        // Detect release: pressed (0) → not pressed (1)
+        if (prev_state == 0 && curr_state == 1) {
+            buzzer_on = !buzzer_on;   // toggle buzzer state
+            gpio_set_level(BUZZER, buzzer_on);
+        }
+        
+        prev_state = curr_state;
+        vTaskDelay(pdMS_TO_TICKS(20));  // debounce
+        }
 }
